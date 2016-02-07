e0f43471458e9f1a2065a8fe7ee21677327248ce
Michael Kapelko <kornerr@gmail.com>
2016-02-07 20:58:18 +0700

    the ball is catched

--- scripts/Ball.py
+++ scripts/Ball.py
@@ -1,27 +1,33 @@
 
 from pymjin2 import *
 
-BALL_ACTION = "spawn.default.rollDown"
+BALL_ACTION        = "spawn.default.rollDown"
+BALL_ACTION_REPORT = "delay.default.ballIsWaiting"
 
 class BallImpl(object):
     def __init__(self, c):
         self.c = c
     def __del__(self):
         self.c = None
+    def onReport(self, key, value):
+        self.c.report("$BALL.$SCENE.$BALL.waiting", value[0])
     def onStopped(self, key, value):
         self.c.report("$BALL.$SCENE.$BALL.moving", "0")
     def setMoving(self, key, value):
-        self.c.set("$MOVE.$SCENE.$BALL.active", "1")
+        self.c.set("$MOVE.$SCENE.$BALL.active", value[0])
 
 class Ball(object):
     def __init__(self, sceneName, nodeName, env):
         self.c = EnvironmentClient(env, "Ball")
         self.impl = BallImpl(self.c)
-        self.c.setConst("SCENE", sceneName)
-        self.c.setConst("BALL",  nodeName)
-        self.c.setConst("MOVE",  BALL_ACTION)
+        self.c.setConst("SCENE",  sceneName)
+        self.c.setConst("BALL",   nodeName)
+        self.c.setConst("MOVE",   BALL_ACTION)
+        self.c.setConst("REPORT", BALL_ACTION_REPORT)
         self.c.provide("$BALL.$SCENE.$BALL.moving", self.impl.setMoving)
+        self.c.provide("$BALL.$SCENE.$BALL.waiting")
         self.c.listen("$MOVE.$SCENE.$BALL.active", "0", self.impl.onStopped)
+        self.c.listen("$REPORT.$SCENE.$BALL.active", None, self.impl.onReport)
     def __del__(self):
         # Tear down.
         self.c.clear()
--- scripts/Main.py
+++ scripts/Main.py
@@ -2,6 +2,7 @@
 from pymjin2 import *
 
 MAIN_BALL_NAME      = "ball"
+MAIN_BALLS_NB       = 4
 MAIN_CLEANER_NAME   = "cleaner"
 MAIN_TRACK_PARTS_NB = 8
 MAIN_PLAIN_NAME     = "plain"
@@ -12,29 +13,72 @@ class MainImpl(object):
         self.c = c
         self.trackPartID = 0
         self.ballInitialPos = None
+        self.ballWaiting = None
+        self.cleanerPicking = None
+        self.ballsLeft = MAIN_BALLS_NB
+        self.ballsCatched = 0
+        self.isCatching = False
     def __del__(self):
         self.c = None
     def onBallStopped(self, key, value):
+        # Ball stopped because of the catch. Do nothing.
+        if (self.isCatching):
+            return
         self.trackPartID = self.trackPartID + 1
         if (self.trackPartID == MAIN_TRACK_PARTS_NB):
-            self.trackPartID = 0
-        self.c.set("node.$SCENE.$BALL.parent",
-                   MAIN_PLAIN_NAME + str(self.trackPartID))
-        self.c.set("node.$SCENE.$BALL.position", self.ballInitialPos)
-        self.rollBall()
+            self.restartBallSequence()
+        else:
+            self.rollBall()
+    def onBallWaiting(self, key, value):
+        self.ballWaiting = None
+        if (value[0] == "1"):
+            self.ballWaiting = self.trackPartID
+        self.tryToCatch()
     def onCatch(self, key, value):
         nodeName = key[2]
         v = nodeName.split(MAIN_CATCH_NAME)
         if (len(v) == 2):
             self.c.set("$CLEANER.$SCENE.$CLEANER.catch", v[1])
     def onCleanerPicking(self, key, value):
-        print "onCleanerPicking", key, value
+        self.cleanerPicking = None
+        if (len(value[0])):
+            self.cleanerPicking = int(value[0])
+        self.tryToCatch()
     def onGameStart(self, key, value):
         print "Game started", key, value
         self.ballInitialPos = self.c.get("node.$SCENE.$BALL.position")[0]
         self.rollBall()
+    def restartBallSequence(self):
+        self.ballsLeft = self.ballsLeft - 1
+        if (self.ballsLeft == 0):
+            print "Game is over"
+            print "Result: catched / overall", self.ballsCatched, MAIN_BALLS_NB
+            if (self.ballsCatched == MAIN_BALLS_NB):
+                print "YOU WON"
+            else:
+                print "YOU LOST"
+        else:
+            self.trackPartID = 0
+            self.rollBall()
     def rollBall(self):
+        self.c.set("node.$SCENE.$BALL.parent",
+                   MAIN_PLAIN_NAME + str(self.trackPartID))
+        self.c.set("node.$SCENE.$BALL.position", self.ballInitialPos)
         self.c.set("ball.$SCENE.$BALL.moving", "1")
+    def stopBall(self):
+        self.c.set("ball.$SCENE.$BALL.moving", "0")
+    def tryToCatch(self):
+        if ((self.ballWaiting is not None) and
+            (self.cleanerPicking is not None) and
+            self.ballWaiting == self.cleanerPicking):
+            self.ballsCatched = self.ballsCatched + 1
+            print "catched the ball at", self.ballWaiting
+            self.isCatching = True
+            self.stopBall()
+            self.isCatching = False
+            # Restart the sequence.
+            self.restartBallSequence()
+            print "catched/left", self.ballsCatched, self.ballsLeft
 
 class Main(object):
     def __init__(self, sceneName, nodeName, env):
@@ -47,6 +91,7 @@ class Main(object):
         self.c.listen("$BALL.$SCENE.$BALL.moving", "0", self.impl.onBallStopped)
         self.c.listen("node.$SCENE..selected", "1", self.impl.onCatch)
         self.c.listen("$CLEANER.$SCENE.$CLEANER.picking", None, self.impl.onCleanerPicking)
+        self.c.listen("$BALL.$SCENE.$BALL.waiting", None, self.impl.onBallWaiting)
     def __del__(self):
         # Tear down.
         self.c.clear()
